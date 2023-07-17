fbaf0a6ea53258fe7bc00fedfe2c036038c223c1
Erlend <49862976+Erb3@users.noreply.github.com>
2023-07-17 21:38:35 +0200

    refactor: Even better mixin (@diskree)

--- src/main/java/github/erb3/fabric/nohotbarlooping/mixin/PlayerInventoryMixin.java
+++ src/main/java/github/erb3/fabric/nohotbarlooping/mixin/PlayerInventoryMixin.java
@@ -2,25 +2,32 @@ package github.erb3.fabric.nohotbarlooping.mixin;
 
 import github.erb3.fabric.nohotbarlooping.NoHotbarLooping;
 import net.minecraft.entity.player.PlayerInventory;
-import net.minecraft.util.math.MathHelper;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(PlayerInventory.class)
 public class PlayerInventoryMixin {
+
+    @Shadow
+    public int selectedSlot;
+
+    @Shadow
+    @Final
+    private static int HOTBAR_SIZE;
+
     @Inject(method="scrollInHotbar", at = @At("HEAD"), cancellable = true)
     public void onScrollInHotbar(double scrollAmount, CallbackInfo ci) {
         if (NoHotbarLooping.shouldLoopHotbar) {
             return;
         }
 
-        assert NoHotbarLooping.client.player != null;
-        PlayerInventory inv = NoHotbarLooping.client.player.getInventory();
-        inv.selectedSlot = MathHelper.clamp(
-                inv.selectedSlot - (int) Math.signum(scrollAmount), 0, 8);
-
-        ci.cancel();
+        int newSlot = this.selectedSlot - (int) Math.signum(scrollAmount);
+        if (newSlot < 0 || newSlot >= HOTBAR_SIZE) {
+            ci.cancel();
+        }
     }
 }
